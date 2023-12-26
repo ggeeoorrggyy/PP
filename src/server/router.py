613a778f512ddde021a08ba51a723a3400480755
ggeeoorrggyy <114288808+ggeeoorrggyy@users.noreply.github.com>
2023-12-26 03:50:55 +0300

    Delete src directory

--- src/server/router.py
+++
@@ -1,6 +0,0 @@
-from src.server.routers import car, order, polomka, post, repair_shop, service, spareparts, staff, users
-
-
-routers = (car.router, order.router, post.router,
-           repair_shop.router, staff.router,
-           polomka.router, users.router, service.router, spareparts.router)
--- src/start_UI.py
+++
@@ -1,5 +0,0 @@
-from src.client.main_window import MainWindow
-
-if __name__ == "__main__":
-    window = MainWindow()
-    window.mainloop()
